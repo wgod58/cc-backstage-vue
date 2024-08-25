32b270c3e19407e04057952d164c08af7530723c
TingHao <tcmbmsaq@gmail.com>
2024-08-25 14:31:01 +0800

    資料呈現，還需補上分頁功能。

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,198 @@
 <script setup>
 import { Menu as IconMenu, Message, Setting } from '@element-plus/icons-vue'
-
 import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import { ElAside, ElButton, ElContainer, ElHeader, ElInput, ElMain } from 'element-plus';
-const item = {
-  date: '2016-05-02',
-  name: 'Tom',
-  address: 'No. 189, Grove St, Los Angeles',
+
+import axios from 'axios';
+const API_URL = "https://metadata.moaifamily.io/clients?page=1&count=10"
+const member = ref([]);
+const totalMember = ref();
+const loadMember = async () => {
+  try {
+    const response = await axios.get(API_URL)
+    member.value = response.data.rows
+    totalMember.value = response.data.totalCount
+    console.log('totalCount:',totalMember.value);
+    for (let i = 0; i < response.data.rows.length; i++) {
+      console.log(response.data.rows[i])
+    }
+
+  } catch (error) {
+    console.error("Error fetching :", error)
+  }
 }
-const tableData = ref(Array.from({ length: 20 }).fill(item))
-const input = ref('')
+loadMember();
+// async function getData() {
+//   const testData = await axios.get(API_URL)
+//   console.log("getData:", testData.data)
+//   return testData.data
+// }
+// async function fetchData() {
+//   const data = await getData();
+//   console.log("fetchData:", data);
+// }
+
+// const item = {
+//   date: '2016-05-02',
+//   name: 'Tom',
+//   address: 'No. 189, Grove St, Los Angeles',
+// }
+// const tableData = ref(Array.from({ length: 20 }).fill(item))
+// const input = ref('')
 
 </script>
 
 <template>
-  <el-container class="layout-container-demo" style="height: 500px">
+  
+  <table border="1">
+    <thead>
+      <tr>
+        <th>birthday</th>
+        <th>canIntroduce</th>
+        <th>comment</th>
+        <th>createdAt</th>
+        <th>delMk</th>
+        <th>email</th>
+        <th>gender</th>
+        <th>headPic</th>
+        <th>id</th>
+        <th>idNo</th>
+        <th>isVip</th>
+        <th>name</th>
+        <th>number</th>
+        <th>phone</th>
+        <th>totalCost</th>
+        <th>updatedAt</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in member" :key="index">
+        <td>{{ item.birthday }}</td>
+        <td>{{ item.canIntroduce }}</td>
+        <td>{{ item.comment }}</td>
+        <td>{{ item.createdAt }}</td>
+        <td>{{ item.delMk }}</td>
+        <td>{{ item.email }}</td>
+        <td>{{ item.gender }}</td>
+        <td>{{ item.headPic }}</td>
+        <td>{{ item.id }}</td>
+        <td>{{ item.idNo }}</td>
+        <td>{{ item.isVip }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.number }}</td>
+        <td>{{ item.phone }}</td>
+        <td>{{ item.totalCost }}</td>
+        <td>{{ item.updatedAt }}</td>
+      </tr>
+    </tbody>
+  </table>
+  <!-- <el-container class="layout-container-demo" style="height: 500px">
     <el-aside width="200px">
       <el-scrollbar>
         <el-menu :default-openeds="['1', '3']">
           <el-sub-menu index="1">
             <template #title>
-              <el-icon><message /></el-icon>Navigator One
+              <el-icon>
+                <message />
+              </el-icon>Navigator One
             </template>
-            <el-menu-item-group>
-              <template #title>Group 1</template>
-              <el-menu-item index="1-1">Option 1</el-menu-item>
-              <el-menu-item index="1-2">Option 2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
-              <el-menu-item index="1-3">Option 3</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="1-4">
-              <template #title>Option4</template>
-              <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-sub-menu index="2">
-            <template #title>
+<el-menu-item-group>
+  <template #title>Group 1</template>
+  <el-menu-item index="1-1">Option 1</el-menu-item>
+  <el-menu-item index="1-2">Option 2</el-menu-item>
+</el-menu-item-group>
+<el-menu-item-group title="Group 2">
+  <el-menu-item index="1-3">Option 3</el-menu-item>
+</el-menu-item-group>
+<el-sub-menu index="1-4">
+  <template #title>Option4</template>
+  <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+</el-sub-menu>
+</el-sub-menu>
+<el-sub-menu index="2">
+  <template #title>
               <el-icon><icon-menu /></el-icon>Navigator Two
             </template>
-            <el-menu-item-group>
-              <template #title>Group 1</template>
-              <el-menu-item index="2-1">Option 1</el-menu-item>
-              <el-menu-item index="2-2">Option 2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
-              <el-menu-item index="2-3">Option 3</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="2-4">
-              <template #title>Option 4</template>
-              <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-sub-menu index="3">
-            <template #title>
-              <el-icon><setting /></el-icon>Navigator Three
+  <el-menu-item-group>
+    <template #title>Group 1</template>
+    <el-menu-item index="2-1">Option 1</el-menu-item>
+    <el-menu-item index="2-2">Option 2</el-menu-item>
+  </el-menu-item-group>
+  <el-menu-item-group title="Group 2">
+    <el-menu-item index="2-3">Option 3</el-menu-item>
+  </el-menu-item-group>
+  <el-sub-menu index="2-4">
+    <template #title>Option 4</template>
+    <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
+  </el-sub-menu>
+</el-sub-menu>
+<el-sub-menu index="3">
+  <template #title>
+              <el-icon>
+                <setting />
+              </el-icon>Navigator Three
             </template>
-            <el-menu-item-group>
-              <template #title>Group 1</template>
-              <el-menu-item index="3-1">Option 1</el-menu-item>
-              <el-menu-item index="3-2">Option 2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
-              <el-menu-item index="3-3">Option 3</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="3-4">
-              <template #title>Option 4</template>
-              <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-        </el-menu>
-      </el-scrollbar>
-    </el-aside>
+  <el-menu-item-group>
+    <template #title>Group 1</template>
+    <el-menu-item index="3-1">Option 1</el-menu-item>
+    <el-menu-item index="3-2">Option 2</el-menu-item>
+  </el-menu-item-group>
+  <el-menu-item-group title="Group 2">
+    <el-menu-item index="3-3">Option 3</el-menu-item>
+  </el-menu-item-group>
+  <el-sub-menu index="3-4">
+    <template #title>Option 4</template>
+    <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+  </el-sub-menu>
+</el-sub-menu>
+</el-menu>
+</el-scrollbar>
+</el-aside>
 
-    <el-container>
-      <el-header style="text-align: right; font-size: 12px">
-        <div class="toolbar">
-          <el-dropdown>
-            <el-icon style="margin-right: 8px; margin-top: 1px">
-              <setting />
-            </el-icon>
-            <template #dropdown>
+<el-container>
+  <el-header style="text-align: right; font-size: 12px">
+    <div class="toolbar">
+      <el-dropdown>
+        <el-icon style="margin-right: 8px; margin-top: 1px">
+          <setting />
+        </el-icon>
+        <template #dropdown>
               <el-dropdown-menu>
                 <el-dropdown-item>View</el-dropdown-item>
                 <el-dropdown-item>Add</el-dropdown-item>
                 <el-dropdown-item>Delete</el-dropdown-item>
               </el-dropdown-menu>
             </template>
-          </el-dropdown>
-          <span>Tom</span>
-        </div>
-      </el-header>
+      </el-dropdown>
+      <span>Tom</span>
+    </div>
+  </el-header>
 
-      <el-main>
-        <el-scrollbar>
-          <el-table :data="tableData">
-            <el-table-column prop="date" label="Date" width="140" />
-            <el-table-column prop="name" label="Name" width="120" />
-            <el-table-column prop="address" label="Address" />
-          </el-table>
-        </el-scrollbar>
-      </el-main>
-    </el-container>
-  </el-container>
+  <el-main>
+    <el-scrollbar>
+      <el-table :data="tableData">
+        <el-table-column prop="birthday" label="birthday" width="140" />
+        <el-table-column prop="canIntroduce" label="canIntroduce" width="140" />
+        <el-table-column prop="comment" label="comment" width="140" />
+        <el-table-column prop="createdAt" label="createdAt" width="140" />
+        <el-table-column prop="delMk" label="delMk" width="140" />
+        <el-table-column prop="email" label="email" width="140" />
+        <el-table-column prop="gender" label="gender" width="140" />
+        <el-table-column prop="headPic" label="headPic" width="140" />
+        <el-table-column prop="id" label="id" width="140" />
+        <el-table-column prop="idNo" label="idNo" width="140" />
+        <el-table-column prop="isVip" label="isVip" width="140" />
+        <el-table-column prop="name" label="name" width="140" />
+        <el-table-column prop="number" label="number" width="140" />
+        <el-table-column prop="phone" label="phone" width="140" />
+        <el-table-column prop="totalCost" label="totalCost" width="140" />
+        <el-table-column prop="updatedAt" label="updatedAt" width="140" />
+      </el-table>
+    </el-scrollbar>
+  </el-main>
+</el-container>
+</el-container> -->
 </template>
 
 <style scoped>
@@ -154,16 +242,20 @@ header {
   background-color: var(--el-color-primary-light-7);
   color: var(--el-text-color-primary);
 }
+
 .layout-container-demo .el-aside {
   color: var(--el-text-color-primary);
   background: var(--el-color-primary-light-8);
 }
+
 .layout-container-demo .el-menu {
   border-right: none;
 }
+
 .layout-container-demo .el-main {
   padding: 0;
 }
+
 .layout-container-demo .toolbar {
   display: inline-flex;
   align-items: center;
