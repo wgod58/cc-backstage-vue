f8e236303e2a6f1d93515d61bdc5a229c67b3db5
TingHao <tcmbmsaq@gmail.com>
2024-08-27 17:04:29 +0800

    Login頁面更新 & Date顯示修正

--- src/views/ClientsView.vue
+++ src/views/ClientsView.vue
@@ -1,278 +1,290 @@
 <template>
-        switch parent border: <el-switch v-model="parentBorder" /> switch child
-        border: <el-switch v-model="childBorder" />
-        <el-table :data="memberData" :border="parentBorder" style="width: 100%">
-          <el-table-column type="expand">
-            <template #default="props" >
-              <div m="4" >
-                <p m="t-0 b-2">State: {{ props.row.state }}</p>
-                <p m="t-0 b-2">City: {{ props.row.city }}</p>
-                <p m="t-0 b-2">Address: {{ props.row.address }}</p>
-                <p m="t-0 b-2">Zip: {{ props.row.zip }}</p>
-                <h3>Family</h3>
-                <el-table :data="props.row.family" :border="childBorder" >
-                  <el-table-column label="cost" prop="cost" />
-                  <el-table-column label="createdAt" prop="createdAt" />
-                </el-table>
-              </div>
-            </template>
-          </el-table-column>
-          <el-table-column label="id" prop="id" />
-          <el-table-column label="number" prop="number" />
-          <el-table-column label="name" prop="name" />
-          <el-table-column label="phone" prop="phone" />
-          <el-table-column label="email" prop="email" />
-          <el-table-column label="gender" prop="gender" />
-          <el-table-column label="birthday" prop="birthday" />
-          <el-table-column label="idNo" prop="idNo" />
-          <el-table-column label="canIntroduce" prop="canIntroduce" />
-          <el-table-column label="isVip" prop="isVip" />
-          <el-table-column label="totalCost" prop="totalCost" />
-          <el-table-column label="headPic" prop="headPic" />
-          <el-table-column label="comment" prop="comment" />
-          <el-table-column label="delMk" prop="delMk" />
-          <el-table-column label="createdAt" prop="createdAt" />
-          <el-table-column label="updatedAt" prop="updatedAt" />
-        </el-table>
+  switch parent border: <el-switch v-model="parentBorder" /> switch child
+  border: <el-switch v-model="childBorder" />
+  <el-table :data="memberData" :border="parentBorder" style="width: 100%">
+    <el-table-column type="expand">
+      <template #default="props">
+        <div m="4">
+          <p m="t-0 b-2">State: {{ props.row.state }}</p>
+          <p m="t-0 b-2">City: {{ props.row.city }}</p>
+          <p m="t-0 b-2">Address: {{ props.row.address }}</p>
+          <p m="t-0 b-2">Zip: {{ props.row.zip }}</p>
+          <h3>Family</h3>
+          <el-table :data="props.row.family" :border="childBorder">
+            <el-table-column label="cost" prop="cost" />
+            <el-table-column label="createdAt" prop="createdAt" />
+          </el-table>
+        </div>
       </template>
-      
-      <script lang="ts" setup>
-      import { ref } from 'vue'
-      import axios from 'axios';
-      const API_URL = 'https://metadata.moaifamily.io/clients?page=1&count=10'
-      const parentBorder = ref(false)
-      const childBorder = ref(false)
-      const memberData = ref([])
-      const loadMember = async() =>{
-        try{
-          const response = await axios.get(API_URL)
-          memberData.value = response.data.rows
-          console.log(response.data.rows)
-        }catch(error){
-          console.error("Error Fetching : ", error)
-        }
-      }
-      loadMember();
-      const tableData = [
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          state: 'California',
-          city: 'San Francisco',
-          address: '3650 21st St, San Francisco',
-          zip: 'CA 94114',
-          family: [
-            {
-              name: 'Jerry',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Spike',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Tyke',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-          ],
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          state: 'California',
-          city: 'San Francisco',
-          address: '3650 21st St, San Francisco',
-          zip: 'CA 94114',
-          family: [
-            {
-              name: 'Jerry',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Spike',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Tyke',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-          ],
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          state: 'California',
-          city: 'San Francisco',
-          address: '3650 21st St, San Francisco',
-          zip: 'CA 94114',
-          family: [
-            {
-              name: 'Jerry',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Spike',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Tyke',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-          ],
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          state: 'California',
-          city: 'San Francisco',
-          address: '3650 21st St, San Francisco',
-          zip: 'CA 94114',
-          family: [
-            {
-              name: 'Jerry',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Spike',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Tyke',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-          ],
-        },
-        {
-          date: '2016-05-08',
-          name: 'Tom',
-          state: 'California',
-          city: 'San Francisco',
-          address: '3650 21st St, San Francisco',
-          zip: 'CA 94114',
-          family: [
-            {
-              name: 'Jerry',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Spike',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Tyke',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-          ],
-        },
-        {
-          date: '2016-05-06',
-          name: 'Tom',
-          state: 'California',
-          city: 'San Francisco',
-          address: '3650 21st St, San Francisco',
-          zip: 'CA 94114',
-          family: [
-            {
-              name: 'Jerry',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Spike',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Tyke',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-          ],
-        },
-        {
-          date: '2016-05-07',
-          name: 'Tom',
-          state: 'California',
-          city: 'San Francisco',
-          address: '3650 21st St, San Francisco',
-          zip: 'CA 94114',
-          family: [
-            {
-              name: 'Jerry',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Spike',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-            {
-              name: 'Tyke',
-              state: 'California',
-              city: 'San Francisco',
-              address: '3650 21st St, San Francisco',
-              zip: 'CA 94114',
-            },
-          ],
-        },
-      ]
-      </script>
-      
-      <style scop>
-    </style>
+    </el-table-column>
+    <el-table-column label="id" prop="id" />
+    <el-table-column label="number" prop="number" />
+    <el-table-column label="name" prop="name" />
+    <el-table-column label="phone" prop="phone" />
+    <el-table-column label="email" prop="email" />
+    <el-table-column label="gender" prop="gender" />
+    <el-table-column label="birthday" prop="birthday" />
+    <el-table-column label="idNo" prop="idNo" />
+    <el-table-column label="canIntroduce" prop="canIntroduce" />
+    <el-table-column label="isVip" prop="isVip" />
+    <el-table-column label="totalCost" prop="totalCost" />
+    <el-table-column label="headPic" prop="headPic" />
+    <el-table-column label="comment" prop="comment" />
+    <el-table-column label="delMk" prop="delMk" />
+    <el-table-column label="createdAt" prop="createdAt" />
+    <el-table-column label="updatedAt" prop="updatedAt" />
+  </el-table>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios';
+const API_URL = 'https://metadata.moaifamily.io/clients?page=1&count=10'
+const parentBorder = ref(false)
+const childBorder = ref(false)
+const memberData = ref([])
+const createdAt = ref()
+const updatedAt = ref()
+
+function convertUTCDateToLocalDate(utcDateStr) {
+  const date = new Date(utcDateStr);
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}
+  -${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
+
+}
+const loadMember = async () => {
+  try {
+    const response = await axios.get(API_URL)
+    for (let i = 0; i < response.data.rows.length; i++) {
+      response.data.rows[i].createdAt = convertUTCDateToLocalDate(response.data.rows[i].createdAt)
+      response.data.rows[i].updatedAt = convertUTCDateToLocalDate(response.data.rows[i].updatedAt)
+    }
+    memberData.value = response.data.rows
+    console.log(response.data.rows)
+  } catch (error) {
+    console.error("Error Fetching : ", error)
+  }
+}
+loadMember();
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+  {
+    date: '2016-05-08',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+  {
+    date: '2016-05-06',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+]
+</script>
+
+<style scop></style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,50 +1,54 @@
 <template>
         <div class="container">
-                <div class="login">
-                        帳號：<el-input v-model="account" style="margin-bottom: 10px; width: 240px" placeholder="請輸入帳號" />
-                        密碼：<el-input v-model="password" style="margin-bottom: 10px;width: 240px" placeholder="請輸入密碼" type="password"/>
-                        <div class="btn-login">
-                                <el-button type="primary" @click="loginCheck">登入</el-button>
+                <form>
+                        <div class="login">
+                                帳號：
+                                <el-input v-model="account" style="margin-bottom: 10px; width: 240px"
+                                        placeholder="請輸入帳號" />
+                                密碼：
+                                <el-input v-model="password" style="margin-bottom: 10px; width: 240px"
+                                        placeholder="請輸入密碼" type="password" />
+                                <div class="btn-login">
+                                        <el-button type="primary" @click="loginCheck">登入</el-button>
+                                </div>
                         </div>
-
-                </div>
-        </div>  
+                </form>
+        </div>
 </template>
-      
-<script lang="ts" setup>
-      import router from '@/router';
-import { ref } from 'vue'
-        const account = ref('account')
-        const password = ref('password')
-        const loginCheck = () => {
-                if(account.value == 'account' && password.value == 'password'){
-                        console.log("LoginSuccess")
-                        router.push('/')
-                }else{
-                        console.log("LoginFailed")
-                }
-        }
 
+<script lang="ts" setup>
+import router from "@/router";
+import { ref } from "vue";
+const account = ref("account");
+const password = ref("password");
+const loginCheck = () => {
+        if (account.value === "account" && password.value === "password") {
+                console.log("LoginSuccess");
+                router.push("/");
+        } else {
+                console.log("LoginFailed");
+        }
+};
 </script>
 
 <style scoped>
-        .container{
-                position: relative;
-                background-color: gray;
-        }
-        .login{
-                text-align: center;
-                border: 1px solid black;
-                border-radius: 5px;
-                padding-top: 20px;
-                padding-left: 10px;
-                position: relative;
-                top:30vh;
-                left: 35vw;
-                height: 25vh;
-                width: 30vw;
-                background-color: #FFBB77;
-        }
+.container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #f5f5f5;
+}
 
+.login {
+        padding: 20px;
+        background-color: white;
+        border-radius: 8px;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+        width: 300px;
+}
+
+.btn-login {
+        text-align: center;
+}
 </style>
-      
